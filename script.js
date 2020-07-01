9571b8307c30ef99e476ba8c2ceeae2c8255eeeb
zacchero <florian.zacchero@helpline.fr>
2020-07-01 14:13:47 +0200

    test token

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
     * dans votre organisation PureCloud. Après avoir créé le client d'octroi implicite, mappez les identifiants client sur
     * la ou les clés de région spécifiées dans l'objet ci-dessous, déployez la page et configurez une application pour pointer vers cette URL.
     */
-    let pcOAuthClientIds = { 'apps.mypurecloud.de': 'd3d0641c-359c-4deb-8723-703f49075de9:_zOFyAjpAnMVDGE3L0j4tlE-pSEvjbc7K7iGSl0xCCg' };
+    let pcOAuthClientIds = { 'mypurecloud.de': 'd3d0641c-359c-4deb-8723-703f49075de9:_zOFyAjpAnMVDGE3L0j4tlE-pSEvjbc7K7iGSl0xCCg' };
     let clientId = pcOAuthClientIds[pcEnvironment];
     if (!clientId) {
         setErrorState(
@@ -254,7 +254,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         return result;
     }
-    
+
     // Demander les trois premiers contacts externes à l'API
     function requestExternalContacts() {
         let apiInstance = new platformClient.ExternalContactsApi();
