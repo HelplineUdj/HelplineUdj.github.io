2850983ec4d7952d2a37820ef993dc799a7d2d1e
zacchero <florian.zacchero@helpline.fr>
2020-07-01 14:23:20 +0200

    test token

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let platformClient = window.require('platformClient');
+document.addEventListener("DOMContentLoaded", function() {
+    let platformClient = window.require("platformClient");
     updateProgressBar(20);
 
     /*
-    * Le SDK des applications clientes peut interpoler l'environnement PC actuel dans l'URL de votre application
-    * EX: https://mypurecloud.github.io/client-app-sdk/profile.html?pcEnvironment={{pcEnvironment}}
-    *
-    * Lecture de l'environnement PC à partir de la chaîne de requête ou du paramètre d'état renvoyé par la réponse OAuth2
-    */
+     * Le SDK des applications clientes peut interpoler l'environnement PC actuel dans l'URL de votre application
+     * EX: https://mypurecloud.github.io/client-app-sdk/profile.html?pcEnvironment={{pcEnvironment}}
+     *
+     * Lecture de l'environnement PC à partir de la chaîne de requête ou du paramètre d'état renvoyé par la réponse OAuth2
+     */
     let pcEnvironment = getEmbeddingPCEnv();
     if (!pcEnvironment) {
         setErrorState(
@@ -18,15 +18,18 @@ document.addEventListener('DOMContentLoaded', function () {
     updateProgressBar(40);
 
     /*
-    * Remarque: Pour utiliser cette application dans votre propre organisation, vous devrez créer vos propres clients OAuth2
-    * dans votre organisation PureCloud. Après avoir créé le client d'octroi implicite, mappez les identifiants client sur
-    * la ou les clés de région spécifiées dans l'objet ci-dessous, déployez la page et configurez une application pour pointer vers cette URL.
-    */
-    let pcOAuthClientIds = { 'de': 'd3d0641c-359c-4deb-8723-703f49075de9:_zOFyAjpAnMVDGE3L0j4tlE-pSEvjbc7K7iGSl0xCCg' };
+     * Remarque: Pour utiliser cette application dans votre propre organisation, vous devrez créer vos propres clients OAuth2
+     * dans votre organisation PureCloud. Après avoir créé le client d'octroi implicite, mappez les identifiants client sur
+     * la ou les clés de région spécifiées dans l'objet ci-dessous, déployez la page et configurez une application pour pointer vers cette URL.
+     */
+    let pcOAuthClientIds = {
+        de:
+            "d3d0641c-359c-4deb-8723-703f49075de9:_zOFyAjpAnMVDGE3L0j4tlE-pSEvjbc7K7iGSl0xCCg",
+    };
     let clientId = pcOAuthClientIds[pcEnvironment];
     if (!clientId) {
         setErrorState(
-            pcEnvironment + ': Unknown/Unsupported PureCloud Environment2'
+            pcEnvironment + ": Unknown/Unsupported PureCloud Environment2"
         );
         return;
     }
@@ -38,42 +41,48 @@ document.addEventListener('DOMContentLoaded', function () {
     let clientApp = null;
     try {
         clientApp = new window.purecloud.apps.ClientApp({
-            pcEnvironment: pcEnvironment
+            pcEnvironment: pcEnvironment,
         });
     } catch (e) {
         setErrorState(
-            pcEnvironment + ': Unknown/Unsupported PureCloud Embed Context'
+            pcEnvironment + ": Unknown/Unsupported PureCloud Embed Context"
         );
         return;
     }
     updateProgressBar(80);
 
     /*
-    * Configurer le testeur manuel maintenant; aucune autorisation PC requise
-    *
-    * Cette interface utilisateur est utilisée à des fins de test pour naviguer manuellement vers un contact externe par ID.
-    * Il ignore délibérément le fait qu'un utilisateur ne peut pas être autorisé à afficher les contacts externes.
-    */
+     * Configurer le testeur manuel maintenant; aucune autorisation PC requise
+     *
+     * Cette interface utilisateur est utilisée à des fins de test pour naviguer manuellement vers un contact externe par ID.
+     * Il ignore délibérément le fait qu'un utilisateur ne peut pas être autorisé à afficher les contacts externes.
+     */
     document
-        .querySelector('.manual-example #contactButton')
-        .addEventListener('click', function () {
-            let contactId = document.querySelector('#contact-id').value.trim();
+        .querySelector(".manual-example #contactButton")
+        .addEventListener("click", function() {
+            let contactId = document.querySelector("#contact-id").value.trim();
             if (contactId) {
-                clientApp.externalContacts.showExternalContactProfile(contactId);
+                clientApp.externalContacts.showExternalContactProfile(
+                    contactId
+                );
             }
         });
     document
-        .querySelector('.manual-example #orgButton')
-        .addEventListener('click', function () {
-            let externalOrganizationId = document.querySelector('#org-id').value.trim();
+        .querySelector(".manual-example #orgButton")
+        .addEventListener("click", function() {
+            let externalOrganizationId = document
+                .querySelector("#org-id")
+                .value.trim();
             if (externalOrganizationId) {
-                clientApp.externalContacts.showExternalOrganizationProfile(externalOrganizationId);
+                clientApp.externalContacts.showExternalOrganizationProfile(
+                    externalOrganizationId
+                );
             }
         });
 
     let redirectUrl = window.location.origin;
     if (!redirectUrl) {
-        redirectUrl = window.location.protocol + '//' + window.location.host;
+        redirectUrl = window.location.protocol + "//" + window.location.host;
     }
     redirectUrl += window.location.pathname;
 
@@ -83,72 +92,92 @@ document.addEventListener('DOMContentLoaded', function () {
 
     client
         .loginImplicitGrant(clientId, redirectUrl, {
-            state: 'pcEnvironment=' + pcEnvironment
+            state: "pcEnvironment=" + pcEnvironment,
         })
-        .then(function () {
+        .then(function() {
             updateProgressBar(100);
             authenticated = true;
             return new platformClient.UsersApi().getUsersMe({
-                expand: ['authorization']
+                expand: ["authorization"],
             });
         })
-        .then(function (profileData) {
+        .then(function(profileData) {
             userDataAcquired = true;
             // Vérifiez si l'utilisateur actuel pourra voir les contacts externes
             let permissions = profileData.authorization.permissions;
-            if (checkPermission(permissions, 'externalContacts:contact:view')) {
-                requestExternalContacts().then(data => {
-                    // Masquer la barre de progression une fois l'authentification terminée et la promesse résolue
-                    let authenticatingEl = document.querySelector('.authenticating');
-                    authenticatingEl.classList.add('hidden');
-                    let entities = data.entities;
-                    // Créer une table avec les données de l'appel API
-                    for (let i = 0; i < entities.length; i++) {
-                        let hasOrg = !!entities[i].externalOrganization;
-                        document.getElementById('tableBody').insertAdjacentHTML('beforeend',
-                            `<tr>
-                                <td>
-                                    <a id="entity-${i}"></a>
-                                </td>
-                                <td>
-                                    <a id="org-${i}"></a>
-                                </td>
-                            </tr>`
+            if (checkPermission(permissions, "externalContacts:contact:view")) {
+                requestExternalContacts()
+                    .then((data) => {
+                        // Masquer la barre de progression une fois l'authentification terminée et la promesse résolue
+                        let authenticatingEl = document.querySelector(
+                            ".authenticating"
                         );
-                        // Attacher des écouteurs à des entités pour appeler des méthodes sdk
-                        let entity = document.getElementById(`entity-${i}`);
-                        entity.addEventListener('click', function () {
-                            clientApp.externalContacts.showExternalContactProfile(entities[i].id);
-                        });
-                        entity.appendChild(document.createTextNode(`${entities[i].lastName}, ${entities[i].firstName}`));
-                        if (hasOrg) {
-                            let org = document.getElementById(`org-${i}`);
-                            let orgId = entities[i].externalOrganization.id;
-                            org.addEventListener('click', function () {
-                                clientApp.externalContacts.showExternalOrganizationProfile(orgId);
+                        authenticatingEl.classList.add("hidden");
+                        let entities = data.entities;
+                        // Créer une table avec les données de l'appel API
+                        for (let i = 0; i < entities.length; i++) {
+                            let hasOrg = !!entities[i].externalOrganization;
+                            document
+                                .getElementById("tableBody")
+                                .insertAdjacentHTML(
+                                    "beforeend",
+                                    `<tr>
+                                        <td>
+                                            <a id="entity-${i}"></a>
+                                        </td>
+                                        <td>
+                                            <a id="org-${i}"></a>
+                                        </td>
+                                    </tr>`
+                                );
+                            // Attacher des écouteurs à des entités pour appeler des méthodes sdk
+                            let entity = document.getElementById(`entity-${i}`);
+                            entity.addEventListener("click", function() {
+                                clientApp.externalContacts.showExternalContactProfile(
+                                    entities[i].id
+                                );
                             });
-                            org.appendChild(document.createTextNode(orgId));
+                            entity.appendChild(
+                                document.createTextNode(
+                                    `${entities[i].lastName}, ${
+                                        entities[i].firstName
+                                    }`
+                                )
+                            );
+                            if (hasOrg) {
+                                let org = document.getElementById(`org-${i}`);
+                                let orgId = entities[i].externalOrganization.id;
+                                org.addEventListener("click", function() {
+                                    clientApp.externalContacts.showExternalOrganizationProfile(
+                                        orgId
+                                    );
+                                });
+                                org.appendChild(document.createTextNode(orgId));
+                            }
                         }
-                    }
-                }).catch(err => {
-                    setErrorState(err);
-                });
+                    })
+                    .catch((err) => {
+                        setErrorState(err);
+                    });
             } else {
-                setErrorState('You do not have the proper permissions to view external contacts.');
+                setErrorState(
+                    "You do not have the proper permissions to view external contacts."
+                );
             }
         })
-        .catch(function () {
+        .catch(function() {
             if (!authenticated) {
-                setErrorState('Failed to Authenticate with PureCloud');
+                setErrorState("Failed to Authenticate with PureCloud");
             } else if (!userDataAcquired) {
-                setErrorState('Failed to locate user in PureCloud');
+                setErrorState("Failed to locate user in PureCloud");
             }
         });
 
     function setErrorState(errorMsg) {
         // Afficher le texte d'erreur dans la barre de progression
-        document.querySelector('.progress-label').innerText = errorMsg;
-        document.querySelector('.progress-bar').className += ' progress-bar-danger';
+        document.querySelector(".progress-label").innerText = errorMsg;
+        document.querySelector(".progress-bar").className +=
+            " progress-bar-danger";
         updateProgressBar(100);
     }
 
@@ -165,16 +194,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (permissionValue.match(/^[a-zA-Z0-9]+:\*$/)) {
-            permissionValue = permissionValue.replace('*', '*:*');
+            permissionValue = permissionValue.replace("*", "*:*");
         }
 
-        const permissionsToValidate = permissionValue.split(':');
+        const permissionsToValidate = permissionValue.split(":");
         const targetDomain = permissionsToValidate[0];
         const targetEntity = permissionsToValidate[1];
         const targetAction = permissionsToValidate[2];
 
-        permissions.forEach(function (permission) {
-            const permissions = permission.split(':');
+        permissions.forEach(function(permission) {
+            const permissions = permission.split(":");
             const domain = permissions[0];
             const entity = permissions[1];
             const actionSet = permissions[2];
@@ -193,7 +222,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function isPermission(item, targetItem) {
-        let isItem = item === '*' || targetItem === '*';
+        let isItem = item === "*" || targetItem === "*";
         if (!isItem) {
             isItem = item === targetItem;
         }
@@ -209,7 +238,10 @@ document.addEventListener('DOMContentLoaded', function () {
     function getEmbeddingPCEnv() {
         let result = null;
 
-        if (window.location.hash && window.location.hash.indexOf('access_token') >= 0) {
+        if (
+            window.location.hash &&
+            window.location.hash.indexOf("access_token") >= 0
+        ) {
             let oauthParams = extractParams(window.location.hash.substring(1));
             if (oauthParams && oauthParams.access_token && oauthParams.state) {
                 // OAuth2 spec dicte cet encodage
@@ -220,7 +252,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (!result && window.location.search) {
-            result = extractParams(window.location.search.substring(1)).pcEnvironment || null;
+            result =
+                extractParams(window.location.search.substring(1))
+                    .pcEnvironment || null;
         }
 
         return result;
@@ -230,15 +264,17 @@ document.addEventListener('DOMContentLoaded', function () {
         let result = {};
 
         if (paramStr) {
-            let params = paramStr.split('&');
-            params.forEach(function (currParam) {
+            let params = paramStr.split("&");
+            params.forEach(function(currParam) {
                 if (currParam) {
-                    let paramTokens = currParam.split('=');
+                    let paramTokens = currParam.split("=");
                     let paramName = paramTokens[0];
                     let paramValue = paramTokens[1];
                     if (paramName) {
                         paramName = decodeURIComponent(paramName);
-                        paramValue = paramValue ? decodeURIComponent(paramValue) : null;
+                        paramValue = paramValue
+                            ? decodeURIComponent(paramValue)
+                            : null;
 
                         if (!result.hasOwnProperty(paramName)) {
                             result[paramName] = paramValue;
@@ -260,15 +296,14 @@ document.addEventListener('DOMContentLoaded', function () {
         let apiInstance = new platformClient.ExternalContactsApi();
         let opts = {
             pageSize: 6,
-            pageNumber: 1
+            pageNumber: 1,
         };
-        return apiInstance.getExternalcontactsContacts(opts)
-            .then(data => {
-                return data;
-            });
+        return apiInstance.getExternalcontactsContacts(opts).then((data) => {
+            return data;
+        });
     }
 
     function updateProgressBar(percent) {
-        document.querySelector('.progress-bar').style.width = `${percent}%`;
+        document.querySelector(".progress-bar").style.width = `${percent}%`;
     }
 });
